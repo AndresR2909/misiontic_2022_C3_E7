781dee26a8714bfdc496242a19fadc1f50692d6d
AndresR2909 <felipeandres29@hotmail.com>
2022-09-03 00:49:11 -0500

    Actulizacion repositorio servicios

--- ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioUsuarios.cs
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioUsuarios.cs
@@ -13,8 +13,9 @@ namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
         {
             usuarios= new List<Usuario>()
             {
-                new Usuario{id=1,nombre="Andres",apellidos= "Restrepo",direccion= "call 23 # 42-13",telefono= "11111113", ciudad="medellin"},
-                new Usuario{id=2,nombre="Juliana",apellidos= "Moncada",direccion= "call 40 # 42-13",telefono= "99999999", ciudad="medellin"}
+                new Usuario{id=1,nombre="Andres",apellidos= "Restrepo",direccion= "calle 23 # 42-13",telefono= "11111113", ciudad="Medellin"},
+                new Usuario{id=2,nombre="Julian",apellidos= "Moncada",direccion= "calle 40 # 42-13",telefono= "99999999", ciudad="Medellin"},
+                new Usuario{id=3,nombre="Pedro",apellidos= "Perez",direccion= "carrera 80 # 2-10",telefono= "2222222", ciudad="Bogota"}
             };
         }
  
